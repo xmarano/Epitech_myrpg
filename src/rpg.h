f7574d3dcd2f51d907ac59f61c8fed49a700d7e0
Prevost <yanis.prevost@epitech.eu>
2024-04-15 10:56:43 +0200

    [ADD] New .h

--- src/rpg.h
+++ src/rpg.h
@@ -5,50 +5,14 @@
 ** rpg.h
 */
 #include "my.h"
+#include "inv.h"
 #include <SFML/Window.h>
 #include <SFML/Graphics.h>
 #include <SFML/Audio.h>
 #include <SFML/Config.h>
+
 #ifndef RPG_H
     #define RPG_H
-
-/* Debut inventaire */
-
-typedef struct inventaire {
-    void *item1;
-    void *item2;
-    void *item3;
-    void *item4;
-    void *item5;
-} inv_t;
-
-typedef struct stats_weapon {
-    int attack;
-    int accuracy;
-    int crit;
-} stweapon_t;
-
-typedef struct stats_mob {
-    int level;
-    int xp;
-    int hp;
-    int lck;
-    int def;
-    int res;
-    int atk;
-    int spd;
-    int mvmt;
-} stmob_t;
-
-typedef struct Perso {
-    char *name;
-    char *type_weapon;
-    stweapon_t stat_w;
-    stmob_t stat_p;
-    inv_t inv;
-}Perso_t;
-
-/* Fin inventaire */
 
 typedef struct Menu {
     sfText *test;
