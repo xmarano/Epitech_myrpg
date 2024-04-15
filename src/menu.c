2c50bf682b5a69dc71f94d6d6098f5e9f862cbe0
xmarano <leo.gregori@epitech.eu>
2024-04-15 13:29:34 +0200

    [MODIFIED] : Init functions

--- src/menu.c
+++ src/menu.c
@@ -4,33 +4,8 @@
 ** File description:
 ** menu.c
 */
-
 #include "rpg.h"
-#include "include/inventory.h"
 #include "include/menu.h"
-
-sfText *init_text(char *str, int size, sfVector2f pos)
-{
-    sfText *text = sfText_create();
-    sfFont *font = sfFont_createFromFile("assets/text.ttf");
-
-    sfText_setFont(text, font);
-    sfText_setCharacterSize(text, size);
-    sfText_setColor(text, sfWhite);
-    sfText_setString(text, str);
-    sfText_setPosition(text, pos);
-    return text;
-}
-
-sfSprite *init_sprite(char *filename, sfVector2f pos)
-{
-    sfTexture *texture = sfTexture_createFromFile(filename, NULL);
-    sfSprite *sprite = sfSprite_create();
-
-    sfSprite_setTexture(sprite, texture, sfTrue);
-    sfSprite_setPosition(sprite, pos);
-    return sprite;
-}
 
 void init_menu(Global_t *m)
 {
