2e21ece49cd86a11a37c76f378f0c52847c325e9
Prevost <yanis.prevost@epitech.eu>
2024-04-15 10:49:56 +0200

    [ADD] inventory stuct

--- src/rpg.h
+++ src/rpg.h
@@ -12,6 +12,44 @@
 #ifndef RPG_H
     #define RPG_H
 
+/* Debut inventaire */
+
+typedef struct inventaire {
+    void *item1;
+    void *item2;
+    void *item3;
+    void *item4;
+    void *item5;
+} inv_t;
+
+typedef struct stats_weapon {
+    int attack;
+    int accuracy;
+    int crit;
+} stweapon_t;
+
+typedef struct stats_mob {
+    int level;
+    int xp;
+    int hp;
+    int lck;
+    int def;
+    int res;
+    int atk;
+    int spd;
+    int mvmt;
+} stmob_t;
+
+typedef struct Perso {
+    char *name;
+    char *type_weapon;
+    stweapon_t stat_w;
+    stmob_t stat_p;
+    inv_t inv;
+}Perso_t;
+
+/* Fin inventaire */
+
 typedef struct Menu {
     sfText *test;
     sfSprite *background;
@@ -21,6 +59,7 @@ typedef struct Glob {
     sfVector2i mouse;
     sfRenderWindow *window;
     Menu_t menu;
+    Perso_t perso;
 } Global_t;
 
 void init_menu(Global_t *m);
