86c32500a91281bbade6cae9a4e70ff51e586e36
xmarano <leo.gregori@epitech.eu>
2024-04-16 11:23:51 +0200

    [ADD] : Cursor sprites & architecture

--- src/menu.c
+++ src/menu.c
@@ -9,19 +9,24 @@
 
 void init_menu(Global_t *m)
 {
-    sfVector2u wsize = sfRenderWindow_getSize(m->window);
-
-    m->menu.background = init_sprite("assets/menu/map_background.png", (sfVector2f){0, 0});
-    m->menu.title = init_text("MY RPG", 150, (sfVector2f){(float)(wsize.x / 2), 25});
+    sfRenderWindow_setMouseCursorVisible(m->window, sfFalse);
+    m->menu.wsize = sfRenderWindow_getSize(m->window);
+    m->menu.map = init_sprite("assets/menu/map.png", (sfVector2f){0, 0});
+    m->menu.cursor = init_sprite("assets/menu/cursor.png", (sfVector2f){0, 0});
+    m->menu.title = init_text("MY RPG", 150, (sfVector2f){m->menu.wsize.x / 2, 25});
 }
 
 void draw_menu(Global_t *m)
 {
-    sfRenderWindow_drawSprite(m->window, m->menu.background, NULL);
+    sfVector2f pos_c = {m->mouse.x, m->mouse.y};
+
+    sfRenderWindow_drawSprite(m->window, m->menu.map, NULL);
+    sfSprite_setPosition(m->menu.cursor, pos_c);
     sfRenderWindow_drawText(m->window, m->menu.title, NULL);
 }
 
 void destroy_menu(Global_t *m)
 {
+    sfSprite_destroy(m->menu.map);
     sfText_destroy(m->menu.title);
 }
