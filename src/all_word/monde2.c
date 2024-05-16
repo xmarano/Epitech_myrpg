18335d80d4abb0a87541d58b2b217e6d7ca91e01
Prevost <yanis.prevost@epitech.eu>
2024-05-16 14:38:23 +0200

    gigi

---
+++ src/all_word/monde2.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** B-MUL-200-MAR-2-1-myrpg-yanis.prevost
+** File description:
+** monde1.c
+*/
+
+#include "../rpg.h"
+#include "../include/perso.h"
+#include "../include/menu.h"
+#include "../include/worlds.h"
+#include "../include/npc.h"
+
+void draw_monde2(Global_t *m, hub_t *hub)
+{
+    if (m->current == 2) {
+
+    }
+}
---
+++ src/all_word/monde5.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2023
+** B-MUL-200-MAR-2-1-myrpg-yanis.prevost
+** File description:
+** monde1.c
+*/
+
+#include "../rpg.h"
+#include "../include/perso.h"
+#include "../include/menu.h"
+#include "../include/worlds.h"
+#include "../include/npc.h"
+
+void draw_monde5(Global_t *m, hub_t *hub)
+{
+    if (m->current == 5) {
+    }
+}
