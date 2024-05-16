ffd6ef67e5cd4b131560470261d1749da7924146
Prevost <yanis.prevost@epitech.eu>
2024-05-16 23:13:36 +0200

    gest err load & save party

--- src/menu/return_menu.c
+++ src/menu/return_menu.c
@@ -8,15 +8,33 @@
 #include "../rpg.h"
 #include "../include/menu.h"
 
+static int gestion_err_du_fichier(FILE *file, int c, Global_t *m)
+{
+    if (file == NULL) {
+        printf("fichier de sauvegarde introuvable!\n");
+        exit(1);
+    }
+    c = fgetc(file);
+    if (c == EOF) {
+        printf("Aucune données de sauvegarde trouvée!\n");
+        fclose(file);
+        return -1;
+    }
+    fseek(file, 0, SEEK_SET);
+}
+
 void load_game(Global_t *m, hub_t *hub)
 {
     FILE *file = fopen("assets/save_party/save.txt", "r");
+    int c;
 
-    if (file == NULL) {
-        exit(EXIT_FAILURE);
+    if (gestion_err_du_fichier(file, c, m) == -1) {
+        m->current = 14;
+        return;
     }
     if (fscanf(file, "%d", &m->gold) != 1) {
-        printf("Erreur\n");
+        printf("Erreur de lecture!\n");
+        sfRenderWindow_close(m->window);
         return;
     }
     m->current = 14;
@@ -28,7 +46,7 @@ void save_game(Global_t *m, hub_t *h)
     FILE *file = fopen("assets/save_party/save.txt", "w");
 
     fprintf(file, "%d", m->gold);
-    printf("game_saved\n");
+    printf("game saved succesfully!\n");
     return;
 }
 
