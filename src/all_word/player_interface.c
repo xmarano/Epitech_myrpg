319d59493792a3a15baeb9aebc85d5ed31e89671
Prevost <yanis.prevost@epitech.eu>
2024-05-21 12:09:14 +0200

    kkk

---
+++ src/all_word/player_interface.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2023
+** B-MUL-200-MAR-2-1-myrpg-yanis.prevost
+** File description:
+** player_interface.c
+*/
+
+#include "../rpg.h"
+#include "../include/perso.h"
+#include "../include/menu.h"
+#include "../include/worlds.h"
+#include "../include/npc.h"
+
+void init_player_interface(Global_t *m)
+{
+    
+}
+
+void draw_player_interface(Global_t *m)
+{
+    printf("caca\n");
+}
