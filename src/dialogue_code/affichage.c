15a26e5d7552922782ca3884f8201b7097dfc63c
Clerc Tom <tom.clerc@epitech.eu>
2024-05-22 12:31:55 +0200

    [ADD] : coding-style

--- src/dialogue_code/affichage.c
+++ src/dialogue_code/affichage.c
@@ -7,28 +7,45 @@
 #include "../rpg.h"
 #include <ctype.h>
 
-const char* ennemi_wall(int current_ennemi) {
+const char *ennemi_wall(int current_ennemi)
+{
     switch (current_ennemi) {
-        case 5: return "assets/perso/dialogue/boss/Boss1_dialogue.png";
-        case 6: return "assets/perso/dialogue/boss/Boss2_dialogue.png";
-        case 7: return "assets/perso/dialogue/boss/Boss3_dialogue.png";
-        case 8: return "assets/perso/dialogue/boss/Boss4_dialogue.png";
-        case 9: return "assets/perso/dialogue/boss/Boss5_dialogue.png";
-        case 10: return "assets/perso/dialogue/boss/Boss6_dialogue.png";
-        case 11: return "assets/perso/dialogue/boss/Boss7_dialogue.png";
-        case 12: return "assets/perso/dialogue/boss/Boss8_dialogue.png";
-        default: return NULL;
+        case 5:
+            return "assets/perso/dialogue/boss/Boss1_dialogue.png";
+        case 6:
+            return "assets/perso/dialogue/boss/Boss2_dialogue.png";
+        case 7:
+            return "assets/perso/dialogue/boss/Boss3_dialogue.png";
+        case 8:
+            return "assets/perso/dialogue/boss/Boss4_dialogue.png";
+        case 9:
+            return "assets/perso/dialogue/boss/Boss5_dialogue.png";
+        case 10:
+            return "assets/perso/dialogue/boss/Boss6_dialogue.png";
+        case 11:
+            return "assets/perso/dialogue/boss/Boss7_dialogue.png";
+        case 12:
+            return "assets/perso/dialogue/boss/Boss8_dialogue.png";
+        default:
+            return NULL;
     }
 }
 
-const char* hero_wall(int current_perso) {
+const char *hero_wall(int current_perso)
+{
     switch (current_perso) {
-        case 0: return "assets/perso/dialogue/hero/Infenium_dialogue.png";
-        case 1: return "assets/perso/dialogue/hero/Patecarbo_dialogue.png";
-        case 2: return "assets/perso/dialogue/hero/Racaillou_dialogue.png";
-        case 3: return "assets/perso/dialogue/hero/Roy_dialogue.png";
-        case 4: return "assets/perso/dialogue/hero/Xmarano_dialogue.png";
-        default: return NULL;
+        case 0:
+            return "assets/perso/dialogue/hero/Infenium_dialogue.png";
+        case 1:
+            return "assets/perso/dialogue/hero/Patecarbo_dialogue.png";
+        case 2:
+            return "assets/perso/dialogue/hero/Racaillou_dialogue.png";
+        case 3:
+            return "assets/perso/dialogue/hero/Roy_dialogue.png";
+        case 4:
+            return "assets/perso/dialogue/hero/Xmarano_dialogue.png";
+        default:
+            return NULL;
     }
 }
 
@@ -78,11 +95,12 @@ void affichage_hero(RenderContext_t *context)
 void draw_dialogue(const char *str, int x, int y, RenderContext_t *context)
 {
     sfText *text = sfText_create();
+    sfVector2f position = {x, y};
+
     sfText_setString(text, str);
     sfText_setFont(text, context->font);
     sfText_setCharacterSize(text, 30);
     sfText_setColor(text, sfBlack);
-    sfVector2f position = {x, y};
     sfText_setPosition(text, position);
     sfRenderWindow_drawText(context->window, text, NULL);
     sfText_destroy(text);
@@ -90,19 +108,20 @@ void draw_dialogue(const char *str, int x, int y, RenderContext_t *context)
 
 void sentencept(char *phrase, RenderContext_t *context, int x, int y)
 {
-    sfTexture* texture = sfTexture_createFromFile("src/dialogue_code/walltom.png", NULL);
+    sfTexture* texture = sfTexture_createFromFile("src/dialogue_code/wall.png", NULL);
     sfSprite* sprite = sfSprite_create();
+    sfVector2f position = {x, y};
+    sfText *text = sfText_create();
+
     sfSprite_setTexture(sprite, texture, sfTrue);
     sfRenderWindow_clear(context->window, sfWhite);
     sfRenderWindow_drawSprite(context->window, sprite, NULL);
     affichage_hero(context);
     affichage_mechant(context);
-    sfText *text = sfText_create();
     sfText_setString(text, phrase);
     sfText_setFont(text, context->font);
     sfText_setCharacterSize(text, 30);
     sfText_setColor(text, sfBlack);
-    sfVector2f position = {x, y};
     sfText_setPosition(text, position);
     sfRenderWindow_drawText(context->window, text, NULL);
     sfRenderWindow_display(context->window);
@@ -136,13 +155,13 @@ void parse_file(char *filename, RenderContext_t *context, Global_t *m)
     char line[256];
     char last_speaker[256] = "";
 
-    if (file == NULL) {
+    if (file == NULL)
         return;
-    }
     while (fgets(line, sizeof(line), file)) {
         if (sfKeyboard_isKeyPressed(sfKeyEscape))
             break;
-        if (verif_parse(line, context, m->perso->current_perso, last_speaker) == 1)
+        if (verif_parse(line, context, m->perso->current_perso, last_speaker)
+        == 1)
             break;
     }
     m->dialogue.start_dialogue = 0;
