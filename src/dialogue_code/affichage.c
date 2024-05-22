ab04c6dbb373fe7fe8bffdaf9c312f9dba59d9e2
Clerc Tom <tom.clerc@epitech.eu>
2024-05-22 09:35:04 +0200

    [ADD] : codinf-style

--- src/dialogue_code/affichage.c
+++ src/dialogue_code/affichage.c
@@ -103,33 +103,68 @@ void sentencept(char *phrase, RenderContext_t *context, int x, int y)
     sfTexture_destroy(texture);
 }
 
+int skip_ennemy(const char *phrase, RenderContext_t *context)
+{
+    int window_width = sfRenderWindow_getSize(context->window).x;
+    int window_height = sfRenderWindow_getSize(context->window).y;
+
+    if (sfKeyboard_isKeyPressed(sfKeyReturn)) {
+        sentencept(phrase, context, window_width - 520, 950);
+        sfSleep(sfSeconds(0.5));
+        return 1;
+    }
+    return 0;
+}
+
+void ennemy_speak(const char *phrase, const char *num, RenderContext_t *context)
+{
+    int window_width = sfRenderWindow_getSize(context->window).x;
+    int window_height = sfRenderWindow_getSize(context->window).y;
+
+    draw_dialogue(num, window_width - 520, window_height - 520, context);
+    sentencept(phrase, context, window_width - 520, 950);
+}
+
+int skip_hero(const char *phrase, RenderContext_t *context)
+{
+    int window_width = sfRenderWindow_getSize(context->window).x;
+    int window_height = sfRenderWindow_getSize(context->window).y;
+
+    if (sfKeyboard_isKeyPressed(sfKeyReturn)) {
+        sentencept(phrase, context, 25, 950);
+        sfSleep(sfSeconds(0.5));
+        return 1;
+    }
+    return 0;
+}
+
+void hero_speak(const char *phrase, const char *num, RenderContext_t *context)
+{
+    int window_width = sfRenderWindow_getSize(context->window).x;
+    int window_height = sfRenderWindow_getSize(context->window).y;
+
+    draw_dialogue(num, 25, window_height - 30, context);
+    sentencept(phrase, context, 25, 950);
+}
+
 void wordpt(char *str, RenderContext_t *context, const char *num, int position)
 {
     char *phrase = malloc(strlen(str) + 1);
-    int window_width = sfRenderWindow_getSize(context->window).x;
-    int window_height = sfRenderWindow_getSize(context->window).y;
+    int should_skip = 0;
 
     strcpy(phrase, "");
     for (int i = 0; i < strlen(str); i++) {
         strncat(phrase, &str[i], 1);
         if (position == 0) {
-            draw_dialogue(num, 25, window_height - 30, context);
-            sentencept(phrase, context, 25, 950);
-            if (sfKeyboard_isKeyPressed(sfKeyReturn)) {
-                sentencept(str, context, 25, 950);
-                sfSleep(sfSeconds(1.0));
-                free(phrase);
-                return;
-            }
-        } else {
-            draw_dialogue(num, window_width - 520, window_height - 520, context);
-            sentencept(phrase, context, window_width - 520, 950);
-            if (sfKeyboard_isKeyPressed(sfKeyReturn)) {
-                sentencept(str, context, window_width - 520, 950);
-                sfSleep(sfSeconds(1.0));
-                free(phrase);
-                return;
-            }
+            hero_speak(phrase, num, context);
+            should_skip = skip_hero(str, context);
+        }
+        if (position != 0){
+            ennemy_speak(phrase, num, context);
+            should_skip = skip_ennemy(str, context);
+        }
+        if (should_skip) {
+            break;
         }
     }
     sfSleep(sfSeconds(2.0));
